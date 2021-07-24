fc8446d73eaa307cb6446c23f598276e525a130b
Tapaswini Tripathy <67327121+Tapaswini15@users.noreply.github.com>
2021-07-24 15:35:22 +0530

    Add files via upload

---
+++ Task3/Student-Result-Management-System-master/contactus.php
@@ -0,0 +1,23 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>Location - Palm Desert</li>
+         	<li>At - CA 92260</li>
+         	<li>United States</li>
+            <li> 74580</li>
+         	<li>Contact no  +1 760-340-5002
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
